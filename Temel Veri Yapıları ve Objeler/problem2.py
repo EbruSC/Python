6f734b7ad860ed31d6e59609b21f0683cf561a9d
EBRU SOMUNCU <57454859+EbruSomuncu@users.noreply.github.com>
2020-05-01 04:58:03 +0300

    Delete problem2.py

--- Temel Veri Yapıları ve Objeler/problem2.py
+++
@@ -1,9 +0,0 @@
-"""
-Kullanıcıdan aldığınız boy ve kilo değerlerine göre kullanıcının beden kitle indeksini bulun.
-
-Beden Kitle İndeksi : Kilo / Boy(m) Boy(m)
-"""
-boy=float(input("boy bilgisi:")) #1.68
-kilo=int(input("kilo bilgisi:"))
-sonuc=kilo/(boy**2)
-print("sonuc:{}".format(sonuc))
