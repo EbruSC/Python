be6fb7cf7cfc05b665f7bc4bbada35746ba22381
EBRU SOMUNCU <57454859+EbruSomuncu@users.noreply.github.com>
2020-05-01 04:48:12 +0300

    Add files via upload

---
+++ Temel Veri Yapıları ve Objeler/problem1.py
@@ -0,0 +1,8 @@
+"""
+KULLANICIDAN ALINAN 3 SAYININ ÇARPILMASI VE FORMAT METODUYLA EKRANDA GÖSTERİLMESİ
+"""
+a=int(input("1.sayı:"))
+b=int(input("2.sayı:"))
+c=int(input("3.sayı:"))
+carp=a*b*c
+print("a:{}*b:{}*c:{}=sonuc:{}".format(a,b,c,carp))
