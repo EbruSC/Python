7a3e7adf80addfb20969651fdcdfb2f05ba0d4d5
EBRU SOMUNCU <57454859+EbruSomuncu@users.noreply.github.com>
2020-05-01 04:57:12 +0300

    Delete problem4.py

--- Temel Veri Yapıları ve Objeler/problem4.py
+++
@@ -1,7 +0,0 @@
-"""
-Kullanıcıdan ad,soyad ve numara bilgisini alarak bunları alt alta ekrana yazdırın.
-"""
-ad=input("Adınız:")
-soyad=input("Soyadınız:")
-numara=input("Numaranız:")
-print("Ad:{}\nSoyad:{}\nNumara:{}".format(ad,soyad,numara))
